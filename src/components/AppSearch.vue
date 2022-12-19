7f58898d9783f55da0cac7e1a8dc9df5216d70d8
furkanGit <furkancaliskan9801@gmail.com>
2022-12-19 14:17:27 +0100

    components AppSearch created

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,28 @@
+<script>
+    import { store } from '../store';
+    export default{
+        data() {
+            return {
+                store,
+            }
+        },
+    }
+</script>
+
+<template>
+
+   <div id="input-container">
+
+    <input type="search" name="" id="input-search"  v-model="store.searchText">
+    <button  @click="$emit('search')">
+        Search
+    </button>
+   </div>
+
+
+
+</template>
+
+<style  lang="scss"  >
+
+</style>
