1e80de16dd02b3c39ff46ec2fca0cef174922356
furkanGit <furkancaliskan9801@gmail.com>
2023-03-24 15:57:16 +0100

    modified store.js

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     movieList: [],
-    apiUrl:"https://api.themoviedb.org/3/movie/popular?api_key=58f2df07dfea14b2ec249e535f5ee506&page=1",
+    tvList: [],
+    moviesAPI: 'https://api.themoviedb.org/3/movie/popular?api_key=6fd82c7e095558dcca8a44519f3dc58a&page=1',
+    tvAPI: 'https://api.themoviedb.org/3/tv/popular?api_key=6fd82c7e095558dcca8a44519f3dc58a&page=1',
     searchText:"",
 });
